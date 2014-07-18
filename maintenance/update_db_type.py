397493713cc337e73f7eb4e808b6e1b8223b052b
Toon Alfrink <toon.alfrink@gmail.com>
2014-07-18 20:01:37 +0200

    update db script

---
+++ maintenance/update_db_type.py
@@ -0,0 +1,14 @@
+"""temp script to make the DB compatible with new code"""
+
+import dbm, pickle
+
+db = dbm.open('scrapers','c')
+for k in db.keys():
+    val = pickle.loads(db[k])
+    if val['run_daily']:
+        val['period'] = 'daily'
+    else:
+        val['period'] = 'never'
+    del val['run_daily']
+    db[k] = val
+    
