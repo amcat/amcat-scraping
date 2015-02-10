56fcbdd3b1a7a3aa4eb972e3459146de8361ea46
Wouter van Atteveldt <wouter@vanatteveldt.com>
2015-02-10 16:59:29 +0100

    move requirements to setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    version='0.10',
+    name="amcatscraping",
+    description="Scrapers for AmCAT",
+    author="Wouter van Atteveldt, Martijn Bastiaan, Toon Alfrink",
+    author_email="wouter@vanatteveldt.com",
+    packages=["amcatscraping"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=[
+        "html2text",
+        "cssselect",
+        "pyamf",
+        "jinja2",
+        "django",
+        "docopt",
+        "lxml",
+        "amcatclient",
+    ],
+    dependency_links = [
+        "https://github.com/amcat/amcatclient#egg=amcatclient",
+    ]
+)
