6fa5316e8af8be2fee5f61ad4a4d880b11c9bdc7
Martijn Bastiaan <martijn@hmbastiaan.nl>
2015-03-01 12:40:45 +0100

    Allow encoding to be supplied (HttpSession)

--- amcatscraping/httpsession.py
+++ amcatscraping/httpsession.py
@@ -27,12 +27,16 @@ class Session(requests.Session):
     """Provides a HTTP session, HTML parsing and a few convenience methods"""
     def __init__(self):
         super(Session, self).__init__()
+        self.encoding = None
         self.headers.update({
             "User-Agent": "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0"
         })
 
     def get_html(self, link, **kwargs):
-        return lxml.html.fromstring(self.get(link, **kwargs).content, base_url=link)
+        content = self.get(link, **kwargs).content
+        if self.encoding:
+            content = content.decode(self.encoding)
+        return lxml.html.fromstring(content, base_url=link)
 
     def get(self, link, **kwargs):
         return super(Session, self).get(link.strip(), **kwargs)
