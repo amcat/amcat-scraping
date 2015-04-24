392f4459dacb7d5bf697aee342a9a881e3388c5f
Martijn Bastiaan <martijn@hmbastiaan.nl>
2015-04-24 11:33:34 +0200

    Fixed FD scraper

--- amcatscraping/scrapers/newspapers/fd.py
+++ amcatscraping/scrapers/newspapers/fd.py
@@ -20,6 +20,7 @@ import collections
 from html2text import html2text
 import lxml.html
 from urlparse import urljoin
+import re
 
 from amcatscraping.article import Article
 from amcatscraping.tools import setup_logging, parse_form
@@ -27,21 +28,31 @@ from amcatscraping.scraper import (LoginMixin, PropertyCheckMixin,
                                    UnitScraper, DateRangeScraper)
 
 
+# This is a magic value stolen from the browser (you can retrieve it using
+# development tools in Chromium / Firefox). I'm not sure how to fetch it
+# programmatically. Its past midnight and I can't be bothered much, so this
+# will have to do for now. I hope it's a non-expiring magic value.
+RXST = "WmNQM0ZYMWlvMU11ZFZWQnA2SG9QZz09"
+
+
+# URLS
 LOGIN_URL = "https://fd.nl/login"
-DIGIKRANT_BASE_URL = "http://fd.nl/digikrant"
-DIGIKRANT_URL = "http://digikrant-archief.fd.nl/vw/edition.do?dp=FD&date=%Y%m%d"
-#DIGIKRANT_URL = "http://digikrant.fd.nl/%Y%m%d/public/"
+BASE_URL = "http://digikrant.fd.nl/"
+ARCHIVE_URL = "http://digikrant-archief.fd.nl/"
+INIT_URL = BASE_URL + "go?url=digikrant-archief.fd.nl/vw/edition.do?dp=FD%26altd=true%26date=%Y%m%d%26ed=00"
+FRONTPAGE_URL = ARCHIVE_URL + "vw/edition.do?dp=FD&altd=true&date=%Y%m%d&ed=00&rxst={rxst}"
+PAGE_URL = ARCHIVE_URL + "vw/page.do?id={id}&pagedisplay=true&date=%Y%m%d"
+ARTICLE_URL = ARCHIVE_URL + "vw/txt.do?id={id}"
 
-SectionEntry = collections.namedtuple("SectionEntry", ["section", "subsection", "pagenr", "date", "url"])
-
-
-def parse_section_url(date, section_url):
-    base_url = date.strftime(DIGIKRANT_URL)
-    return urljoin(base_url, section_url.replace("%PATH%", "/public")) + ".html"
+# REs
+ARTICLE_ID_RE = re.compile("FD-[0-9-]+")
 
 
 class FinancieelDagbladScraper(LoginMixin, PropertyCheckMixin, UnitScraper, DateRangeScraper):
     def login(self, username, password):
+        # Set right cookies
+        self.session.get(BASE_URL)
+
         login_page = self.session.get(LOGIN_URL)
         login_doc = lxml.html.fromstring(login_page.content)
         login_form = login_doc.cssselect("form.login")[0]
@@ -53,59 +64,66 @@ class FinancieelDagbladScraper(LoginMixin, PropertyCheckMixin, UnitScraper, Date
         post_data.update({"username": username, "password": password})
         response = self.session.post(login_post_url, post_data)
 
-        # Set right cookies
-        self.session.get(DIGIKRANT_BASE_URL)
-
         # Check if logging in worked :)
         return response.url != login_fail_url
 
     def _get_section_table(self, page):
         return (line.strip() for line in page if line.strip().startswith("sectionTable.add"))
 
-    def _get_sections(self, date):
-        # Sections are defined in javascript, like so:
-        # sectionTable.add('Katern 3', 'Outlook', '1', '/pages/07001/FD-07-001-20150207');
-        frontpage = self.session.get(date.strftime(DIGIKRANT_URL)).content
+    def _get_pages(self, date):
+        container = self.session.get_html(date.strftime(FRONTPAGE_URL).format(rxst=RXST))
+        frame_url = container.cssselect("#pageframe")[0].get("src")
+        absolute_frame_url = urljoin(ARCHIVE_URL, frame_url)
 
-        print(frontpage)
+        doc = self.session.get_html(absolute_frame_url)
+        pages = doc.cssselect("#selectPage option")
 
-        for section in self._get_section_table(frontpage.split("\n")):
-            section_tuple = section.lstrip("sectionTable.add(").rstrip(");").replace("'", "")
-            section, subsection, pagenr, url = section_tuple.split(", ")
-            yield SectionEntry(section, subsection, pagenr, date, parse_section_url(date, url))
+        for page in pages:
+            page_nr, section = page.text.split("-")
+            page_nr = page_nr.strip()[1:]
+            section = section.strip()
+            page_url = date.strftime(PAGE_URL).format(id=page.get("value"))
+            yield page_nr, section, page_url
 
-    def _get_articles(self, entry):
-        page_doc = self.session.get_html(entry.url)
+    def _get_articles(self, date, url):
+        page_doc = self.session.get_html(url)
 
-        page_map = page_doc.cssselect("map[name=pagemap]")[0]
-        for area in page_map.cssselect("area"):
-            _, url, _ = area.get('onclick').split("'")
-            yield "%s_body.html" % urljoin(entry.url, url).rstrip(".html")
+        for td in page_doc.cssselect("td[onclick]"):
+            aids = ARTICLE_ID_RE.findall(td.get("onclick"))
+            yield date.strftime(ARTICLE_URL).format(id=aids[0])
 
     def get_units(self):
         for date in self.dates:
-            for entry in self._get_sections(date):
-                for article_url in set(self._get_articles(entry)):
-                    _entry = collections.OrderedDict(entry._asdict(), url=article_url)
-                    yield SectionEntry(*_entry.values()), self.session.get_html(article_url)
+            for page_nr, section, page_url in self._get_pages(date):
+                for article_url in set(self._get_articles(date, page_url)):
+                    yield page_nr, section, date, article_url
 
-    def scrape_unit(self, entry_and_article):
-        entry, article = entry_and_article
+    def scrape_unit(self, props):
+        page_nr, section, date, url = props
+
+        article = self.session.get_html(url)
+
+        text = article.cssselect("td > font.artbody")
+        if not text:
+            return None
 
         text = article.cssselect("td > font.artbody")[1:]
         text = map(html2text, map(lxml.html.tostring, text))
         text = "\n\n".join(p.replace("\n", "") for p in text)
 
         article_properties = {
-            "headline": article.cssselect("title")[0].text,
-            "text": text
+            "headline": article.cssselect("td.artheader")[0].text,
+            "subsection": article.cssselect("td.artlabel")[0].text,
+            "pagenr": page_nr,
+            "section": section,
+            "text": text,
+            "date": date,
+            "url": url
         }
 
         author = article.cssselect(".artauthor")
         if author:
             article_properties["author"] = author[0].text.split(":")[1].strip()
-
-        article_properties.update(entry._asdict())
 
         if article_properties["text"].strip():
             return Article(article_properties)
@@ -115,11 +133,6 @@ class FinancieelDagbladScraper(LoginMixin, PropertyCheckMixin, UnitScraper, Date
             'medium': 'Financieel Dagblad',
             'insertscript': 'FinancieelDagbladScraper'
         },
-        'required': ['subsection', 'date', 'headline', 'section', 'pagenr', 'text'],
+        'required': ['date', "subsection", 'headline', 'section', 'pagenr', 'text'],
         'expected': []
     }
-
-
-if __name__ == '__main__':
-    setup_logging()
-    FinancieelDagbladScraper().run()
