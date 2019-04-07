bfcaa2967584dc0050fcfce0d394f7a445a70673
Martijn Bastiaan <martijn@hmbastiaan.nl>
2019-04-07 10:13:32 +0200

    Rewrote NRC scraper for new version of NRC website

--- amcatscraping/scrapers/nrc.py
+++ amcatscraping/scrapers/nrc.py
@@ -17,114 +17,149 @@
 # License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
 ###########################################################################
 
-import datetime
-from urllib import parse
-
 import collections
-import lxml.html
+import datetime
+import json
+import locale
 import re
 
 from amcat.models import Article
-from amcatscraping.tools import setup_logging, parse_form, listify
-from amcatscraping.scraper import LoginMixin, UnitScraper, DateRangeScraper, \
-    Units
+from amcatscraping.scraper import UnitScraper, DateRangeScraper, \
+    Units, SeleniumMixin, SeleniumLoginMixin
+from amcatscraping.tools import html2text
 
-OVERVIEW_URL = "https://login.nrc.nl/overview"
+AUTHOR_RE = re.compile('"author": \[{"name": "(?P<name>[^"]+)", "@type": "Person"}')
 
-PUBLISHED_PREFIX = "Dit artikel werd gepubliceerd in"
-PUBLISHED_POSTFIX = " (?P<paper>[\.\w ]+) op (?P<date>[\w ,]+), pagina (?P<page>[\w -]+)"
-PUBLISHED_RE = re.compile(PUBLISHED_PREFIX + PUBLISHED_POSTFIX)
-
-ArticleTuple = collections.namedtuple("ArticleTuple", ["url", "date"])
+ArticleTuple = collections.namedtuple("ArticleTuple", ["url", "date", "page"])
 
 
-class NRCScraper(LoginMixin, UnitScraper, DateRangeScraper):
-    nrc_version = None
+def dutch_strptime(date, pattern):
+    loc = locale.getlocale()
+    locale.setlocale(locale.LC_ALL, 'nl_NL.UTF-8')
+    try:
+        return datetime.datetime.strptime(date, pattern)
+    finally:
+        locale.setlocale(locale.LC_ALL, loc)
 
-    def login(self, username, password):
-        login_page = self.session.get(OVERVIEW_URL)
-        login_doc = lxml.html.fromstring(login_page.content)
-        login_url = login_page.url
 
-        post = parse_form(login_doc.cssselect("#fm1")[0])
-        post.update({"username": username, "password": password})
-        post.update({"rememberMe": "on"})
+PAGE_NR_RE = re.compile("#\d+$")
+def strip_pagenr(url):
+    if PAGE_NR_RE.search(url):
+        (url, _) = PAGE_NR_RE.split(url)
+    return url
 
-        response = self.session.post(login_url, post)
 
-        return response.url.endswith("/overview")
+class NRCScraper(SeleniumLoginMixin, SeleniumMixin, DateRangeScraper,
+                 UnitScraper):
+    publisher = None
+    edition = None
 
-    @listify(wrapper=Units)
-    def get_units(self):
-        for date in self.dates:
-            for doc in self.__getsections(date):
-                for a in doc.cssselect("ul.article-links li > a"):
-                    yield ArticleTuple(parse.urljoin(a.base_url, a.get('href')), date)
+    cookies_ok_button = "CookiesOK cookie-bar__button"
+    krant_url = "https://www.nrc.nl/{edition}/{year}/{month:02d}/{day:02d}/"
+    login_url = "https://login.nrc.nl/login"
+    login_username_field = "#username"
+    login_password_field = "#password"
+    login_error_selector = ".feedback.fout > p"
+    allow_missing_login = False
+
+    def __init__(self, *args, **kwargs):
+        super(NRCScraper, self).__init__(*args, **kwargs)
+        self.seen_urls = set()
+
+    def is_valid_url(self, url):
+        if "/index/" in url:
+            return False
+
+        if not url.startswith("https://www.nrc.nl/"):
+            return False
+
+        return True
 
     def get_url_from_unit(self, unit: ArticleTuple) -> str:
         return unit.url
 
-    def __getsections(self, date):
-        monthminus = date.month - 1
-        url1 = "http://digitaleeditie.nrc.nl/digitaleeditie/{self.nrc_version}/{date.year}/{monthminus}/{date.year}{date.month:02d}{date.day:02d}___/section1.html".format(**locals())
-        doc1 = self.session.get_html(url1)
-        yield doc1
-        for a in doc1.cssselect("ul.main-sections li:not(.active) a.section-link"):
-            yield self.session.get_html(parse.urljoin(a.base_url, a.get("href")))
+    def _get_urls(self, date):
+        url = self.krant_url.format(
+            edition=self.edition,
+            year=date.year,
+            month=date.month,
+            day=date.day
+        )
 
-    def scrape_unit(self, unit):
-        url = unit.url
-        doc = self.session.get_html(url)
-        text = "\n\n".join([t.text_content() for t in doc.cssselect("em.intro,div.column-left p")])
+        self.browser.get(url)
 
-        if not text:
-            return None
+        if self.browser.current_url == url:
+            pages = self.wait(".paper__pages")
+            for page in pages.find_elements_by_css_selector(".c-page"):
+                pagenr = page.get_attribute("id")[len("page-"):]
+                for article in page.find_elements_by_css_selector(
+                        "a.c-page__article"):
+                    url = strip_pagenr(article.get_attribute("href"))
+                    if self.is_valid_url(url):
+                        yield ArticleTuple(url, date, pagenr)
 
-        datestr = url.split("/")[7].strip("_")
-        location = doc.cssselect("em.location")
-        person = doc.cssselect("p.by span.person")
+    def _dedup_urls(self, tups):
+        seen_urls = set()
+        for (url, date, page) in tups:
+            if url in seen_urls:
+                continue
+            seen_urls.add(url)
+            yield ArticleTuple(url, date, page)
 
-        published = doc.cssselect(".published")[0].text_content().strip()
-        published = PUBLISHED_RE.match(published).groupdict()
+    def get_units(self):
+        for date in self.dates:
+            yield Units(
+                self._dedup_urls(
+                    self._get_urls(date)
+                )
+            )
 
-        try:
-            pagenr = int(url.split("/")[8].split("_")[1])
-        except IndexError:
-            pagenr = int(published["page"].split("-")[0])
+    def get_meta(self, tag):
+        return (
+            self.browser
+                .find_element_by_css_selector("meta[property='{}']".format(tag))
+                .get_attribute("content")
+        )
 
-        section_span = doc.cssselect("#Content ul.main-sections li.active span")
-        if section_span:
-            section = section_span[0].text
-        else:
-            section = doc.cssselect("#Content .breadcrums a.previous")[0].text
+    def scrape_unit(self, unit: ArticleTuple):
+        (url, date, page) = unit
 
-        date = datetime.datetime(*map(int, [datestr[:4], datestr[4:6], datestr[6:]]))
-        title = doc.cssselect("#MainContent h2")[0].text_content().strip() or "-"
+        self.browser.get(url)
+
+        url = self.browser.current_url
+        title = self.get_meta("og:title")
+        subject = self.get_meta("ad:subject")
+        categories = self.get_meta("ad:categories")
+        pagenr = int(page[1:])
+
+        author = None
+        author_re = AUTHOR_RE.search(self.browser.page_source)
+        if author_re:
+            author = author_re.groupdict()["name"].strip()
+            author = json.loads('"{}"'.format(author))
+
+        article = self.browser.find_element_by_css_selector(".content.article__content")
+        article_html = article.get_attribute("innerHTML")
+        text = html2text(article_html)
 
         article = Article(date=date, title=title, text=text, url=url)
+        article.set_property("pagenr_int", pagenr)
+        article.set_property("page", page)
+        article.set_property("subject", subject)
+        article.set_property("categories", categories)
 
-        author = person and person[0].text_content() or None
-        if author:
+        if author is not None:
             article.set_property("author", author)
 
-        location = location and location[0].text or None
-        if location:
-            article.set_property("location", location)
-
-        subtitle = "\n".join([h3.text_content() for h3 in doc.cssselect("div.column-left h3")])
-
-        article.set_property("subtitle", subtitle)
-        article.set_property("section", section)
-        article.set_property("pagenr_int", pagenr)
-
-        return article
-
-
-class NRCHandelsbladScraper(NRCScraper):
-    publisher = "NRC Handelsblad"
-    nrc_version = "NH"
-
+        if url not in self.seen_urls:
+            self.seen_urls.add(url)
+            return article
 
 class NRCNextScraper(NRCScraper):
     publisher = "NRC.NEXT"
-    nrc_version = "NN"
+    edition = "next"
+
+class NRCHandelsbladScraper(NRCScraper):
+    publisher = "NRC Handelsblad"
+    edition = "handelsblad"
+
